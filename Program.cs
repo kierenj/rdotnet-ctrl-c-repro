4c3992780ed7f7e910c80e19400a0488f42b8d84
Kieren Johnstone <kierenj@gmail.com>
2019-06-20 08:15:03 +0100

    repro commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace r_ctrl_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stop = new ManualResetEvent(false);
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                Console.WriteLine("CTRL-C!");
+                stop.Set();
+                e.Cancel = true; // we'll handle outselves for this demo
+            };
+
+            RDotNet.REngine.GetInstance();
+
+            Console.WriteLine("Started...");
+            stop.WaitOne();
+            Console.WriteLine("Stopped");
+        }
+    }
+}
